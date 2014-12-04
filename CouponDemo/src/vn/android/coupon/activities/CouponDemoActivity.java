e475e1e9f90dc1dcb2264ad0bf721628b811a975
RamAndSpice <ramandspice@gmail.com>
2014-12-04 00:06:46 -0500

    again

--- CouponDemo/src/vn/android/coupon/activities/CouponDemoActivity.java
+++ CouponDemo/src/vn/android/coupon/activities/CouponDemoActivity.java
@@ -8,7 +8,7 @@ public class CouponDemoActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//test//test
+        super.onCreate(savedInstanceState);//test//test//test
         loadUrl("file:///android_asset/www/login.html");
     }
 }
